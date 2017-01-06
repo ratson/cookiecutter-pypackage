9be5f71f104a5f2938903f51443d7021eedb2b47
Ratson <github@ratson.name>
2017-01-06 10:20:41 +0800

    Initial commit

---
+++ {{cookiecutter.package_name}}/README.rst
@@ -0,0 +1,25 @@
+{{ cookiecutter.package_name }}
+{{ cookiecutter.package_name|count * "=" }}
+
+.. image:: https://img.shields.io/pypi/v/{{ cookiecutter.package_name }}.svg
+    :target: https://pypi.python.org/pypi/{{ cookiecutter.package_name }}
+    :alt: Latest PyPI version
+    :crossorigin: anonymous
+
+{{ cookiecutter.package_description }}
+
+Requirements
+------------
+
+Installation
+------------
+
+You can install ``{{ cookiecutter.package_name }}`` via `pip`_ from `PyPI`_::
+
+    $ pip install {{ cookiecutter.package_name }}
+
+.. _`pip`: https://pypi.python.org/pypi/pip/
+.. _`PyPI`: https://pypi.python.org/pypi
+
+Usage
+-----
---
+++ {{cookiecutter.package_name}}/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+setuptools.setup(
+    name="{{ cookiecutter.package_name }}",
+    version="{{ cookiecutter.package_version }}",
+    url="{{ cookiecutter.package_url }}",
+
+    author="{{ cookiecutter.author_name }}",
+    author_email="{{ cookiecutter.author_email }}",
+
+    description="{{ cookiecutter.package_description }}",
+    long_description=open('README.rst').read(),
+    keywords=[],
+
+    packages=setuptools.find_packages(),
+
+    install_requires=[],
+
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
